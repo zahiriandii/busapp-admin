f217ef3682f5d1228f80f98886af3a9594a6c713
Andi Zahiri <zahiriandii@gmail.com>
2025-12-09 15:16:56 +0100

    project init

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Your pages will go here
+const routes = [
+  // Example: { path: '/', component: HomePage }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
